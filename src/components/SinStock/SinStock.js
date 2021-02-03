ebf3198c0dd64fbafc715fe9788bb2f0774e93af
Gabrielmaceira <gabrielmaceira@gmail.com>
2021-02-03 12:01:31 -0300

    Fix formato SinStock

--- src/components/SinStock/SinStock.js
+++ src/components/SinStock/SinStock.js
@@ -3,7 +3,7 @@ import { Col } from 'react-bootstrap'
 
 export const SinStock = ({stock, size}) => {
   if (Number(stock) <= 0) {
-    return <Col xs={Number(size)} className='noStock p-0 m-0'> Sin stock </Col>
+    return <Col xs={Number(size)} className='noStock m-auto'> Sin stock </Col>
   }
   else {
     return null
